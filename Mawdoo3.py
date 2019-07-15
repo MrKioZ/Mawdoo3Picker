45fc33dfcbafee021a95f46dd5fff6ca14e53475
Mr.Kio <38723412+MrKioZ@users.noreply.github.com>
2019-07-15 09:48:26 -0700

    OOP Implemented to the Script to be more organized

--- Mawdoo3.py
+++ Mawdoo3.py
@@ -3,41 +3,44 @@ from random import choice
 from bs4 import BeautifulSoup
 from random import choice
 from random import shuffle
-import os
+
+class mawdoo():
+
+    def __init__(self):
+        self.url = 'https://mawdoo3.com/'
+        self.website = get(self.url).content
+        self.content = BeautifulSoup(self.website, 'html.parser')
+        self.categories = self.content.find_all("div", {"class":"category"})
+        self.scrapped_categories = []
+        self.scrapped_articles = []
+
+    def GetArticle(self):
+
+        for category in self.categories:
+            for category_item in category.find_all('li'):
+                if not category_item.find('a')['href'].startswith('http'):
+                    self.scrapped_categories.append(category_item.find('a')['href'])
+        shuffle(self.scrapped_categories)
+        self.Choosen_Category = self.url + choice(self.scrapped_categories)
+
+        self.website = get(self.Choosen_Category).content
+        self.soup = BeautifulSoup(self.website, 'html.parser')
+        self.articles = self.soup.find("ul", {'class': "row categories-list"}).find_all('a')
+        self.scrapped_articles = []
+
+        for article in self.articles:
+            if not article['href'].startswith("http"):
+                self.scrapped_articles.append(article['href'])
+
+        shuffle(self.scrapped_articles)
+        self.OpenArticle(self.url+choice(self.scrapped_articles))
+
+    def OpenArticle(self, url):
+        """Opens a new tab"""
+        """It uses a the default"""
+        import webbrowser
+        webbrowser.open_new(url)
 
 
-url = 'https://mawdoo3.com/'
-website = get(url)
-website = website.content
-soup = BeautifulSoup(website, 'html.parser')
-
-
-#Picks a random category
-scrapped_categories = []
-categories = soup.find_all("div", {"class":"category"})
-for category in categories:
-    for category_item in category.find_all('li'):
-        if not category_item.find('a')['href'].startswith('http'):
-            scrapped_categories.append(category_item.find('a')['href'])
-
-shuffle(scrapped_categories)
-Choosen_Category = url+choice(scrapped_categories)
-
-#Picks a ranadom Article from the Category
-website = get(Choosen_Category)
-website = website.content
-soup = BeautifulSoup(website, 'html.parser')
-articles = soup.find("ul", {'class': "row categories-list"})
-articles = articles.find_all('a')
-scrapped_articles = []
-
-for article in articles:
-    if not article['href'].startswith("http"):
-            scrapped_articles.append(article['href'])
-
-shuffle(scrapped_articles)
-Choosen_Article = url+choice(scrapped_articles)
-
-#Shows the Choosen Article to the user by opening Firefox
-os.chdir("C:\Program Files\Mozilla Firefox")
-os.system('firefox.exe '+Choosen_Article)
+if __name__ == '__main__':
+    mawdoo().GetArticle()
